b09fcc9ce4b14f3f8d8b6164283d2ddf6b4848a4
sachin1830 <sachinkumarbiswal18@gmail.com>
2021-02-03 16:15:24 +0530

    code modular

--- src/test/FirstTest.java
+++ src/test/FirstTest.java
@@ -6,20 +6,53 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirstTest {
 
-	public static void main(String[] args) 
-	{
+	public static String webbrowser;
+	public static WebDriver driver;
+
+	public static void main(String[] args) {
 		
-		System.setProperty("webdriver.chrome.driver", "/media/sachin/New Volume1/SeleniumTest/SeleniumProgram/Selenium jar and driver/drivers/chromedriver/chromedriver");
-		WebDriver driver=new ChromeDriver();
+		setBrowser("chrome");
+		browserConfig();
+		runTest();
+
+	}
+	
+	
+	//this method set the webbrowser
+
+	public static void setBrowser(String browser) {
+		webbrowser = browser;
+
+	}
+
+	
+	//This method used for browser configuration
+	
+	public static void browserConfig() {
+		if (webbrowser.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver",
+					"/media/sachin/New Volume1/SeleniumTest/SeleniumProgram/Selenium jar and driver/drivers/chromedriver/chromedriver");
+			driver = new ChromeDriver();
+		} else if (webbrowser.equals("firefox")) {
+			
+			// this is relative path getproperty() return the path of the project and we are
+			// using that path
+
+			String location = System.getProperty("user.dir");
+			System.setProperty("webdriver.gecko.driver",
+					location + "/Selenium jar and driver/drivers/firefoxdriver/geckodriver");
+			 driver = new FirefoxDriver();
+		}
 		
-		//this is relative path getproperty() return the path of the project and we are using that path
-		
-		//This is for the firefox
-		
-//		String location=System.getProperty("user.dir");
-//		System.setProperty("webdriver.gecko.driver",location+"/Selenium jar and driver/drivers/firefoxdriver/geckodriver");
-//		WebDriver driver=new  FirefoxDriver();
-		
+		else {
+			System.out.println("Something went wrong!!");
+		}
+
+	}
+	
+	//This method fire the query
+
+	public static void runTest() {
 		driver.get("https://www.selenium.dev/");
 	}
 
